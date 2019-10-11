6a598a44695282c7bc9e59ba6706fc78cb97bb82
Ayur Arkhipov <gangrue@gmail.com>
2019-10-11 21:41:29 +0800

    Initial Commit

---
+++ TourneySearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TourneySearch
+//
+//  Created by ayur on 11.10.2019.
+//  Copyright © 2019 ayur-team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
